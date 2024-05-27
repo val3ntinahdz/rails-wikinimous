33a5270cc60c537c1fe6350fd1d156b18b44724c
your_github_username <valentinaleonh8@gmail.com>
2024-05-27 15:29:35 -0600

    wikinimous in process

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    @article.update(article_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", function() {
+  var editor = ace.edit("ace-editor");
+  editor.setTheme("ace/theme/solarized_light");
+  editor.session.setMode("ace/mode/javascript");
+  editor.setOptions({
+    maxLines: Infinity,
+    showLineNumbers: true,
+    showGutter: true,
+    useSoftTabs: true,
+    tabSize: 2
+  });
+
+  // Sincronizar contenido del editor con el campo oculto
+  var hiddenField = document.getElementById('article_content');
+  editor.session.on('change', function() {
+    hiddenField.value = editor.getValue();
+  });
+
+  // Establecer el valor inicial del editor desde el campo oculto
+  editor.setValue(hiddenField.value);
+});
